11e482d0dad4dbdad60118a2f338bbae5fd24371
Vatsal Parikh <131516495+parikhvinay@users.noreply.github.com>
2023-04-26 00:18:51 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -23,12 +23,12 @@ parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 # sidebar for navigation
 with st.sidebar:
     
-    selected = option_menu('Multiple Disease Prediction System',
+    selected = option_menu('Heart Disease Prediction System',
                           
-                          ['Diabetes Prediction',
+                          [
                            'Heart Disease Prediction',
-                           'Parkinsons Prediction'],
-                          icons=['activity','heart','person'],
+                          ],
+                          icons=[,'heart'],
                           default_index=0)
     
     
